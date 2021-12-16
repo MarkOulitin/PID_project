ba2da0bd9476bb7746cd8a449a243275fe892c6b
Lior Pevzner <liorpev@post.bgu.ac.il>
2021-12-16 11:53:42 +0200

    OPC Client Attempt

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            {
+                client.Connect();
+
+                var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                Browse(node);
+
+                //OpcValue value = client.ReadNode("ns=2;i=2");
+                //if (value.Status.IsGood)
+                //{
+                //    double intvalue = (double)value.Value;
+                //    Console.WriteLine("value", intvalue);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("bad");
+                //}
+            }
+        }
+        private static void Browse(OpcNodeInfo node, int level = 0)
+        {
+            Console.WriteLine("{0}{1}({2})",
+                    new string('.', level * 4),
+                    node.Attribute(OpcAttribute.DisplayName).Value,
+                    node.NodeId);
+
+            level++;
+
+            foreach (var childNode in node.Children())
+                Browse(childNode, level);
+        }
+    }
+}

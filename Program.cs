41b043e89c3c68365eb23feeeb8b1f9566ed3a2f
Mark Oulitin <fragoric@gmail.com>
2021-12-18 11:20:11 +0200

    Added Math component and polling procedure

--- Program.cs
+++ Program.cs
@@ -3,77 +3,92 @@ using System.Net.Sockets;
 using Opc.UaFx;
 using Opc.UaFx.Client;
 using System.Collections.Generic;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+//using System.Text.Json;
+// https://www.newtonsoft.com/json/help/html/T_Newtonsoft_Json_Linq_JValue.htm
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
 namespace ConsoleApp2
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
             Console.WriteLine("Started...");
-            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            HttpClient httpClient = createClient();
+            using (var opcClient = new OpcClient("opc.tcp://localhost:4840"))
             {
                 Console.WriteLine("Connecting...");
-                client.Connect();
+                opcClient.Connect();
                 Console.WriteLine("Connected!");
-                OpcNodeInfo node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                OpcNodeInfo node = opcClient.BrowseNode(OpcObjectTypes.ObjectsFolder);
                 // PLC_L1_43_P19.L1_43_P19.425026@short
                 List<OpcNodeInfo> plcs = new List<OpcNodeInfo>();
-                bool flag = false;
-                foreach (OpcNodeInfo childNode in node.Children())
-                {
-                    if (!flag)
-                    {
-                        flag = true;
-                        continue;
-                    }
-                    foreach (OpcNodeInfo sensor in childNode.Children())
-                    {
-                        OpcValue data = client.ReadNode(sensor.NodeId);
-                        Console.WriteLine("Name: {0} ID: {1} Value: {2} DataType: {3}",
-                            sensor.Name, 
-                            sensor.NodeId, 
-                            data,
-                            data.DataType);
-                    }
-                    //if (childNode.Name.ToString().StartsWith("PLC"))
-                    //{
-                    //    plcs.Add(childNode);
-                    //}
-                }
-
-
-                //Browse(node);
-                //var some = client.BrowseNodes();
-                //var e = some.GetEnumerator();
-                //Console.WriteLine(e.Current);
-                //Console.WriteLine("--------------------------");
-                //e.MoveNext();
-                //Console.WriteLine(e.Current);
-                //OpcValue value = client.ReadNode("ns=2;i=2");
-                //if (value.Status.IsGood)
-                //{
-                //    double intvalue = (double)value.Value;
-                //    Console.WriteLine("value", intvalue);
-                //}
-                //else
-                //{
-                //    Console.WriteLine("bad");
-                //}
+                IEnumerator<OpcNodeInfo> plc_enumerator = node.Children().GetEnumerator();
+                plc_enumerator.MoveNext(); // Now point on first child
+                plc_enumerator.MoveNext();
+                OpcNodeInfo plc = plc_enumerator.Current;
+                Console.WriteLine("Name: {0} ID: {1}",
+                    plc.Name,
+                    plc.NodeId);
+                IEnumerator<OpcNodeInfo> sensors_enumerator = plc.Children().GetEnumerator();
+                sensors_enumerator.MoveNext();
+                OpcNodeInfo sensor = sensors_enumerator.Current;
+                Tuple<List<DateTime>, List<Double>> samples = sample(5, sensor, opcClient);
+                findInflectionGradient(httpClient, samples);
             }
+            httpClient.Dispose();
         }
-        private static void Browse(OpcNodeInfo node, int level = 0)
+
+        private static async void findInflectionGradient(HttpClient httpClient, Tuple<List<DateTime>, List<Double>> samples)
         {
-            Console.WriteLine("{0}{1}({2})",
-                    new string('.', level * 4),
-                    node.Attribute(OpcAttribute.DisplayName).Value,
-                    node.NodeId);
+            string data = prepareJson(samples);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await httpClient.PostAsync("http://127.0.0.1:5000/", content);
+            string responseString = await response.Content.ReadAsStringAsync();
+            JObject response_json = JObject.Parse(responseString);
+            Console.WriteLine(response_json);
+        }
 
-            level++;
+        private static HttpClient createClient()
+        {
+            return new HttpClient();
+        }
 
-            foreach (var childNode in node.Children())
-                Browse(childNode, level);
+        private static string prepareJson(Tuple<List<DateTime>, List<Double>> samples)
+        {
+            JObject data = new JObject();
+            data.Add("time", new JArray(samples.Item1));
+            data.Add("output", new JArray(samples.Item2));
+            return data.ToString(Formatting.None);
+        }
+
+        private static Tuple<List<DateTime>, List<Double>> sample(int n, OpcNodeInfo sensor, OpcClient client)
+        {
+            Tuple<List<DateTime>, List<Double>> samples = Tuple.Create(new List<DateTime>(), new List<Double>());
+            for (int i = 0; i < n; i++)
+            {
+                Tuple<DateTime, Double> data_point = (poll(client, sensor));
+                samples.Item1.Add(data_point.Item1);
+                samples.Item2.Add(data_point.Item2);
+                Thread.Sleep(100);
+            }
+            return samples;
+        }
+        private static Tuple<DateTime, Double> poll(OpcClient client, OpcNodeInfo sensor)
+        {
+            DateTime request_time = DateTime.Now;
+            OpcValue data = client.ReadNode(sensor.NodeId);
+
+            return Tuple.Create(
+                data.ServerTimestamp != null ? (DateTime)data.ServerTimestamp : 
+                data.SourceTimestamp != null ? (DateTime)data.SourceTimestamp : request_time,
+                (Double)data.Value);
         }
     }
 }

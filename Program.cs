a92a7a5abc491b9dbb22150d6498451d99b7b0c4
Tomer <nagarcode@gmail.com>
2021-12-20 18:31:59 +0200

    CLI

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace ConsoleApp2
         public static string opc_port = "4840";
         public static string MathAPI_port = "localhost";
         public static string System_port = "localhost";
+        private static int QUIT = -1;
         private static SamplePool _pool = null;
         public static SamplePool pool
         {
@@ -40,9 +41,14 @@ namespace ConsoleApp2
                 ConnectClient(client);
                 List<OpcNodeInfo> plcs = discoverPLCS(client);
                 List<OpcNodeId> plc_ids = ExtractPlcIDs(plcs);
+                //---------------------- Delete: -----------------------------
+                //List < OpcNodeInfo > plcs= new();
+                //List<OpcNodeId> plc_ids = new();
+                //plc_ids.Add(new OpcNodeId(new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 }));
+                //--------------------- /Delete: -----------------------------
                 InitPool(plc_ids);
                 Sampler sampler = Sampler.Create(opc_url, plcs[0], pool);
-                cli(plc_ids);
+                cliAsync(plc_ids, plcs, client);
             }
         }
 
@@ -75,25 +81,126 @@ namespace ConsoleApp2
 
         }
 
-        private static int cli(List<OpcNodeId> plc_ids)
+        private static async void cliAsync(List<OpcNodeId> plc_ids, List<OpcNodeInfo> plcs, OpcClient client)
         {
-            //var app = new CommandLineApplication();
+            int chosenIndex = 0;
+            while(chosenIndex != -1)
+            {
+                chosenIndex = InputLoop(plc_ids);
+                if(chosenIndex == -1)
+                {
+                    break;
+                }
+                double setPoint = promtSetpointLoop();
+                OpcNodeId chosenNode = plc_ids[chosenIndex];
+                PIDTask task = PIDTask.Create(chosenNode, setPoint, pool);
+                (double, double, double) recommendation = await task.recommendPID(); //TODO should await?
+                //(double, double, double) recommendation = (1.1, 1.2, 1.3);
+                PrintRecommendation(recommendation);
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+                Console.WriteLine();
+            }
 
-            //app.HelpOption();
-            //var subject = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
-            ////subject.De = "world";
 
-            //app.OnExecute(() =>
-            //{
-            //    Console.WriteLine($"Hello {subject.Value()}!");
-            //    return 0;
-            //});
+        }
 
+        private static void PrintRecommendation((double, double, double) recommendation)
+        {
+            double p = recommendation.Item1;
+            double i = recommendation.Item2;
+            double d = recommendation.Item3;
+            Console.WriteLine("Recommended P value: {0}", p);
+            Console.WriteLine("Recommended I value: {0}", i);
+            Console.WriteLine("Recommended D value: {0}", d);
+        }
 
+        private static double promtSetpointLoop()
+        {
+            bool validUserInput = false;
+            double userInput = -1;
+            while (validUserInput == false)
+            {
+                try
+                {
+                    userInput = PromtsetpointInput();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input must be a number. Try again.");
+                    continue;
+                }
 
+                if (IsValidSetpointInput(userInput))
+                {
+                    validUserInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
 
-            //return app.Execute();
-            return 0;
+            }
+            return userInput;
+        }
+
+        private static bool IsValidSetpointInput(double userInput)
+        {
+            return true; //TODO needs further validation?
+        }
+
+        private static double PromtsetpointInput()
+        {
+            double userInput;
+            Console.WriteLine("Enter setpoint:  ");
+            userInput = double.Parse(Console.ReadLine());
+            return userInput;
+        }
+
+        private static int InputLoop(List<OpcNodeId> plc_ids)
+        {
+            int intUserInput = 0;
+            bool validUserInput = false;
+            while (validUserInput == false)
+            {
+                try
+                {
+                    intUserInput = PromtUserInput(plc_ids);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input must be a number. Try again.");
+                    continue;
+                }
+
+                if (IsValidInput(intUserInput, plc_ids.Count))
+                {
+                    validUserInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Chosen index out of bounds. Try again.");
+                }
+
+            }
+            return intUserInput;
+        }
+
+        private static int PromtUserInput(List<OpcNodeId> plc_ids)
+        {
+            int idIndex = 0;
+            int intUserInput;
+            Console.WriteLine("Choose a controller, or type {0} to quit:", QUIT);
+            plc_ids.ForEach(id => {
+                Console.WriteLine("{0}. {1}", idIndex, id.ToString());
+            });
+            intUserInput = int.Parse(Console.ReadLine());
+            return intUserInput;
+        }
+
+        private static bool IsValidInput(int intUserInput, int listLength)
+        {
+            return intUserInput == QUIT || (intUserInput >= 0 && intUserInput < listLength);
         }
 
         private static List<OpcNodeInfo> discoverPLCS(OpcClient client)

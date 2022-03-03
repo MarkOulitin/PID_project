14247dacd782e301f9be7e4984a44e59e315271d
tomsandalon <tomsandalon@gmail.com>
2022-03-03 21:26:00 +0200

    added server tests

--- server/server.py
+++ server/server.py
@@ -5,16 +5,31 @@ import simulator_mock
 from dao.dal import DB
 
 app = Flask(__name__)
-db = DB()
-simulator = simulator_mock.Simulator()
+
+
+class Server:
+    def __init__(self, db=DB(), simulator=simulator_mock.Simulator()):
+        self.db = db
+        self.simulator = simulator
+
+    def query(self, query_request):
+        self.db.create_request(query_request)
+        result = self.simulator.simulate(query_request)
+        return result
+
+    def query_endpoint(self):
+        query_request = queryrequest.flask_request_to_request(request)
+        result = self.query(query_request)
+        return jsonify(result.__dict__)
+
+
+server = Server()
 
 
 @app.route(rule='/', methods=('GET', 'POST'))
-def query():
-    flask_request_to_request = queryrequest.flask_request_to_request(request)
-    # db.create_request(flask_request_to_request)
-    result = simulator.simulate(flask_request_to_request)
-    return jsonify(result.__dict__)
+def act():
+    return server.query_endpoint()
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()

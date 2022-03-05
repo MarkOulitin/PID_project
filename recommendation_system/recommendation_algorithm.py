0252d0105f89a23aa1acf06f19b8462ed021361a
tomsandalon <tomsandalon@gmail.com>
2022-03-05 18:15:28 +0200

    Refactored interpolation of ziegler nichols

--- recommendation_system/recommendation_algorithm.py
+++ recommendation_system/recommendation_algorithm.py
@@ -1,8 +1,12 @@
+from typing import List
+
+from sample import Sample
+
 
 class Algorithm:
     def __init__(self):
         pass
 
-    def recomend(self):
+    def recomend(self, samples: List[Sample]):
         pass
     

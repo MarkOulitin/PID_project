d785e1f3898ae3aba0efcbc5216461d36cf08c42
tomsandalon <tomsandalon@gmail.com>
2022-03-05 18:15:28 +0200

    Refactored interpolation of ziegler nichols

--- recommendation_system/algorithm_impl/ziegler_nochols.py
+++ recommendation_system/algorithm_impl/ziegler_nochols.py
@@ -1,19 +1,54 @@
 from numpy import gradient
 from recommendation_system.recommendation_algorithm import Algorithm
 from recommendation_types import PID
-from typing import Tuple
+from typing import Tuple, List
 from scipy import interpolate, optimize
 
+from sample import Sample
+
 SLOPE_NEAR_ZERO = "Slope approaches zero"
+
+
+def normalize_data(data):
+    return {
+        "output": data["output"],
+        "time": list(map(lambda time: time - min(data["time"]), data["time"]))
+    }
+
+
+def interpolate_data(xs, ys, slope_thresh=10 ** (-3)):
+    # plt.plot(xs, ys)
+    # plt.show()
+    func = interpolate.UnivariateSpline(xs, ys)
+    derivate_once = func.derivative(n=1)
+    derivated_twice = derivate_once.derivative(n=1)
+    x_root_of_second_derivative = optimize.root(derivated_twice, x0=np.array([[1.]]))['x'][0]
+    point = (x_root_of_second_derivative, func(x_root_of_second_derivative).item())
+    slope = derivate_once(x_root_of_second_derivative).item()
+    if abs(slope) < slope_thresh:
+        return None, None, SLOPE_NEAR_ZERO
+    return point, slope, None
+
+
+def samples_to_data_dict(samples: List[Sample]) -> dict:
+    time = list(map(lambda sample: sample.timestamp, samples))
+    output = list(map(lambda sample: sample.process_value, samples))
+    return {
+        "time": time,
+        "output": output
+    }
+
 
 class ZieglerNichols(Algorithm):
 
     def __init__(self, set_point):
         super().__init__()
-    
-    def recomend(self):
-        normalized_data = self.normalize_data(time_to_millis(data))
-        inflection_point, gradient, err_message = self.interpolate_data(normalized_data["time"], normalized_data["output"])
+        self.set_point = set_point
+
+    def recomend(self, samples: List[Sample]):
+        data = samples_to_data_dict(samples)
+        normalized_data = normalize_data(data)
+        inflection_point, gradient, err_message = interpolate_data(normalized_data["time"], normalized_data["output"])
         return self.calculate_pid(inflection=inflection_point, gradient= gradient)
     
     def calculate_pid(self, inflection, gradient):
@@ -32,21 +67,4 @@ class ZieglerNichols(Algorithm):
         return PID(K_p, K_i, K_d)
         
     # TODO make it safer for data
-    def interpolate_data(self, xs, ys, slope_thresh=10 ** (-3)):
-        # plt.plot(xs, ys)
-        # plt.show()
-        func = interpolate.UnivariateSpline(xs, ys)
-        derivate_once = func.derivative(n=1)
-        derivated_twice = derivate_once.derivative(n=1)
-        x_root_of_second_derivative = optimize.root(derivated_twice, x0=np.array([[1.]]))['x'][0]
-        point = (x_root_of_second_derivative, func(x_root_of_second_derivative).item())
-        slope = derivate_once(x_root_of_second_derivative).item()
-        if abs(slope) < slope_thresh:
-            return None, None, SLOPE_NEAR_ZERO
-        return point, slope, None
-    
-    def normalize_data(self, data):
-        new_data = {}
-        new_data["output"] = data["output"]
-        new_data["time"] = list(map(lambda time: time - min(data["time"]), data["time"]))
-        return new_data
+

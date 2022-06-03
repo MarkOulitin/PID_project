007a707463906b7580a92534660849629555116d
Lior Pevzner <liorpev@post.bgu.ac.il>
2022-06-03 18:33:57 +0300

    algo fix

--- my-app/src/components/Algorithms/Algorithms.tsx
+++ my-app/src/components/Algorithms/Algorithms.tsx
@@ -9,49 +9,26 @@ export const Algorithms: React.FC<AlgorithmProps> = ({
 	names,
 	algorithmIndex,
 }) => {
-	const [checked, setChecked] = React.useState(
-		new Array(names.length).fill(false)
-	);
-
-	React.useEffect(() => {
-		const copyChecked = [...checked];
-		copyChecked[0] = true;
-		setChecked(copyChecked);
-	}, []);
-
-	const handleChange = (
-		e: React.ChangeEvent<HTMLInputElement>,
-		index: number
-	) => {
-		const copyChecked = Array(names.length).fill(false);
-		copyChecked[index] = e.target.checked;
-		if (copyChecked.every((value) => !value)) {
-			algorithmIndex(0);
-			copyChecked[0] = true;
-		} else {
-			algorithmIndex(index);
-		}
-		setChecked(copyChecked);
-	};
+	const [checked, setChecked] = React.useState(0);
 
 	return (
 		<Container>
 			<Box sx={{ display: "flex", flexDirection: "row", ml: 3 }}>
-				{names.map((name: string, index: number) => {
-					return (
-						<FormControlLabel
-							label={name}
-							control={
-								<Checkbox
-									checked={checked[index]}
-									onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-										handleChange(e, index)
-									}
-								/>
-							}
-						/>
-					);
-				})}
+				{names &&
+					names.map((name: string, index: number) => {
+						return (
+							<FormControlLabel
+								key={name}
+								label={name}
+								control={
+									<Checkbox
+										checked={index === checked ? true : false}
+										onChange={() => setChecked(index)}
+									/>
+								}
+							/>
+						);
+					})}
 			</Box>
 		</Container>
 	);

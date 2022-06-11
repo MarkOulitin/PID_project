18d878d581a7b0627cb439be9f087d8dbad1e9b2
tomsandalon <tomsandalon@gmail.com>
2022-06-11 15:06:42 +0300

    test custom algorithm skeleton

---
+++ tests/external_algoritm_tests/external_algorithms_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class ExternalAlgorithmTests(unittest.TestCase):
+    def test_fetch_algorithm(self):
+        self.assertEqual(True, False)
+
+    def test_save_algorithm(self):
+        self.assertEqual(True, False)
+
+    def test_run_algorithm(self):
+        self.assertEqual(True, False)
